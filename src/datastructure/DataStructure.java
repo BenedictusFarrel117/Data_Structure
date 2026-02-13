62e7a041642f773d56f4b61b47ab7028681e3c00
Lenovo <Lenovo@LAPTOP-CH6K1PS9>
2026-02-13 13:56:19 +0700

    first commit

---
+++ src/datastructure/DataStructure.java
@@ -0,0 +1,12 @@
+package datastructure;
+public class DataStructure {
+    public static void main(String[] args) {
+        int i = 100;
+        int b = 50;
+        
+        int l = i / b;
+        
+        System.out.println(l);
+    }
+    
+}
