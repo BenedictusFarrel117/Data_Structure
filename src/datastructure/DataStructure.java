e7bfaff20f5bfce30bc9a5c5763fe0f6905c11c9
Lenovo <Lenovo@LAPTOP-CH6K1PS9>
2026-02-13 14:23:10 +0700

    second commit

--- src/datastructure/DataStructure.java
+++ src/datastructure/DataStructure.java
@@ -1,12 +1,7 @@
 package datastructure;
 public class DataStructure {
     public static void main(String[] args) {
-        int i = 100;
-        int b = 50;
-        
-        int l = i / b;
-        
-        System.out.println(l);
+        System.out.println("Hello world!!");
     }
     
 }
