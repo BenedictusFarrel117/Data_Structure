ff758a702b1f149546607ffd79a6d6b9647873ee
Bintang <Bintang@LAPTOP-U1SCN356>
2026-02-13 15:59:02 +0700

    first commit

--- src/datastructure/DataStructure.java
+++ src/datastructure/DataStructure.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package datastructure;
-
-/**
- *
- * @author Lenovo
- */
 public class DataStructure {
     public static void main(String[] args) {
         int [] nilai = {1,2,3,4,5,6,7,8,9,10};
+        double total = 0;
+        for (int i = 0; i<nilai.length; i++){
+            total+=nilai[i];
+        }
         
     }
 }
